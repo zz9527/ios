6ee823f2561faee2d0e675495f27ad648d0d1a90
net_zh110@126.com <net_zh110@126.com>
2016-03-12 20:44:59 +0800

    实现iconButton按钮上下左右移动功能

--- ButtonAction/ButtonAction/ViewController.m
+++ ButtonAction/ButtonAction/ViewController.m
@@ -8,11 +8,47 @@
 
 #import "ViewController.h"
 
+/*宏定义 移动偏移量*/
+#define kMovingDelta 20.f;
+
+/*枚举上下左右*/
+typedef enum{
+    kMovingDirTop=10,
+    kMovingDirBottom,
+    kMovingDirLeft,
+    kMovingDirRight
+
+} kMovingDir;
+
+
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *iconButton;
 
 @end
 
 @implementation ViewController
+
+/*按钮移动*/
+- (IBAction)move:(UIButton *)sender {
+    
+    CGRect frame=self.iconButton.frame;
+    switch (sender.tag) {
+        case kMovingDirTop://向上移动
+            frame.origin.y-=kMovingDelta;
+            break;
+        case kMovingDirBottom://向下移动
+            frame.origin.y+=kMovingDelta;
+            break;
+        case kMovingDirLeft://向左移动
+            frame.origin.x-=kMovingDelta;
+            break;
+        case kMovingDirRight://向右移动
+            frame.origin.x+=kMovingDelta;
+            break;
+    }
+    self.iconButton.frame=frame;
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
