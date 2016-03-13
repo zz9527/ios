2ba39e8969c33d923958a85e3d5a1c76220d40f8
net_zh110@126.com <net_zh110@126.com>
2016-03-13 15:39:24 +0800

    增加缩放功能；首尾动画

--- ButtonAction/ButtonAction/ViewController.m
+++ ButtonAction/ButtonAction/ViewController.m
@@ -28,9 +28,79 @@ typedef enum{
 
 @implementation ViewController
 
+-(IBAction)zoom:(UIButton*)sender{
+    
+    /*bounds缩放*/
+    CGRect bounds= self.iconButton.bounds;
+    if (sender.tag) {
+        NSLog(@"放大");
+        bounds.size.width+=20;
+        bounds.size.height+=20;
+    }
+    else
+    {
+        NSLog(@"缩小");
+        bounds.size.width-=20;
+        bounds.size.height-=20;
+    }
+    
+    /*加入首尾动画*/
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:1];
+    
+    self.iconButton.bounds=bounds;
+    
+    [UIView commitAnimations];
+
+    /*frame缩放*/
+    /*
+    CGRect frame=self.iconButton.frame;
+    if (sender.tag) {
+        NSLog(@"放大");
+        frame.size.width+=20;
+        frame.size.height+=20;
+    }
+    else
+    {
+        NSLog(@"缩小");
+        frame.size.width-=20;
+        frame.size.height-=20;
+    }
+    
+    self.iconButton.frame=frame;
+    */
+
+}
+
+
 /*按钮移动*/
 - (IBAction)move:(UIButton *)sender {
     
+    /*center实现按钮移动*/
+    CGPoint center=self.iconButton.center;
+    switch (sender.tag) {
+        case kMovingDirTop://向上移动
+            center.y-=kMovingDelta;
+            break;
+        case kMovingDirBottom://向下移动
+            center.y+=kMovingDelta;
+            break;
+        case kMovingDirLeft://向左移动
+            center.x-=kMovingDelta;
+            break;
+        case kMovingDirRight://向右移动
+            center.x+=kMovingDelta;
+            break;
+    }
+    
+    /*首尾动画*/
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:1];
+    
+    self.iconButton.center=center;
+    [UIView commitAnimations];
+    /*
+     
     CGRect frame=self.iconButton.frame;
     switch (sender.tag) {
         case kMovingDirTop://向上移动
@@ -47,8 +117,12 @@ typedef enum{
             break;
     }
     self.iconButton.frame=frame;
+     
+    */
     
 }
+
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
